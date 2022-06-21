1ada8ff913a285881cc5deaa60daf4d162eb3dfa
insa257 <77707172+insa257@users.noreply.github.com>
2022-06-20 19:02:56 -0600

    Create int k, get user input, print k strings

--- Subset.java
+++ Subset.java
@@ -1,3 +1,7 @@
+/**
+ * Subset reads in an integer k and N strings from the keyboard. 
+ * It then prints out k random strings from the input strings.
+ */
 package a02;
 
 import edu.princeton.cs.algs4.*;
@@ -6,15 +10,25 @@ public class Subset
 {
 	public static void main(String[] args)
 	{
-		  RandomizedQueue<String> queue = new RandomizedQueue<String>();
-	        int size = 3;
-	        while (!StdIn.isEmpty()) {
-	            queue.enqueue(StdIn.readString());
-	        }
-	        while (size > 0) {
-	            System.out.println(queue.dequeue());
-	            size--;
-	        }
+		// Use RandomizedQueue to get a random output:
+		RandomizedQueue<String> queue = new RandomizedQueue<String>();
+		
+		StdOut.print("Provide the number of items you would like to output: ");
+		int k = StdIn.readInt();
+		
+		StdOut.println();
+		StdOut.print("Input the strings you would like to add to the queue: ");
+		while(!StdIn.isEmpty())
+		{
+			queue.enqueue(StdIn.readString());
+		}
+		
+		StdOut.println();
+		StdOut.println("Here is the output: ");
+		for (int i = 0; i < k; i++)
+		{
+			StdOut.println(queue.dequeue());
+		}
 	}
 }
  
